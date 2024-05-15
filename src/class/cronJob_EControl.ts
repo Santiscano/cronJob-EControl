745e186f808690ad5a349e80cf3ecc9ec380721f
santiago sierra cano <santiscano@gmail.com>
2024-05-15 11:12:37 -0500

    cron job e-control

---
+++ src/class/cronJob_EControl.ts
@@ -0,0 +1,27 @@
+import cron from 'node-cron';
+import puppeteer from 'puppeteer';
+
+export class CronJobEControl {
+  constructor(){}
+
+  async visitUrl(url:string){
+    // un cron que se ejecute cada minuto y haga un console.log()
+    cron.schedule('* * * * *', async () => {
+
+      console.log(`a las ${new Date()}, visito la url: ${url}`)
+      
+      const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });
+      
+      const page = await browser.newPage();
+      await page.goto(url);
+
+      setTimeout(async () => {
+        await browser.close();
+      }, 1000 * 55)
+
+    });
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+// crons
+import { CronJobEControl } from "./class/cronJob_EControl";
+
+const cronJobEControl = new CronJobEControl();
+
+cronJobEControl.visitUrl('https://desarrolloenviexpress.site/api_package/go-exito/');
+cronJobEControl.visitUrl('https://enviexpress.app/scanner.php');
+cronJobEControl.visitUrl('https://www.e-control.com.co/api/evidences/imgebox.php');
